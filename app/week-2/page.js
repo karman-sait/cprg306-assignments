62e949f72a5a14334091229891090e5cdae9798c
karman-sait <karmanveer.singh@edu.sait.ca>
2024-10-08 22:17:25 -0600

    testing

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,9 +1,10 @@
-import Link from 'next/link';
-export default function Home() {
+import StudentInfo from './student-info';
+
+export default function Page() {
   return (
     <main>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="https://github.com/karman-sait/cprg306-assignments/tree/master/app/week-2">Week 2 Assignment</Link>
+      <h1>Shopping List</h1>
+      <StudentInfo />
     </main>
   );
 }
