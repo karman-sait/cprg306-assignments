dcd77a1c5f26f8a83d1ebb8f23cddd1e095686c3
karman-sait <karmanveer.singh@edu.sait.ca>
2024-10-08 21:45:41 -0600

    student-info.js created

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,7 +1,9 @@
-export default function Page() {
-    return (
-      <main>
-        <h1>Shopping List</h1>
-      </main>
-    );
-  }
+import Link from 'next/link';
+export default function Home() {
+  return (
+    <main>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <Link href="https://github.com/karman-sait/cprg306-assignments/tree/master/app/week-2">Week 2 Assignment</Link>
+    </main>
+  );
+}
---
+++ app/week-2/student-info.js
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Karmanveer Singh</p>
+      <Link href="https://github.com/karman-sait/cprg306-assignments">GitHub Repository</Link>
+    </div>
+  );
+}
