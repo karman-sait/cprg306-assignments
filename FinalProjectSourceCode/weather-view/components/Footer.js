647dac46ded0adc69909a70a217becb6be4f2ed0
karman-sait <karmanveer.singh@edu.sait.ca>
2024-12-10 22:41:09 -0700

    deleted troublesome folder will submit seperately

--- FinalProjectSourceCode/weather-view/components/Footer.js
+++
@@ -1,8 +0,0 @@
-"use client";
-export default function Footer() {
-  return (
-    <footer className="bg-gray-900 text-white p-4 text-center text-sm">
-      © {new Date().getFullYear()} WeatherView. Powered by OpenWeather.
-    </footer>
-  );
-}
--- FinalProjectSourceCode/weather-view/components/LoadingSpinner.js
+++
@@ -1,8 +0,0 @@
-"use client";
-export default function LoadingSpinner() {
-  return (
-    <div className="flex justify-center items-center my-4">
-      <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-blue-600"></div>
-    </div>
-  );
-}
